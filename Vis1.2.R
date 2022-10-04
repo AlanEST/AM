2c180769b8f8c6532ecca48db851528f3ca5c00b
AlanEST <113927155+AlanEST@users.noreply.github.com>
2022-10-04 15:22:37 -0500

    Add files via upload

---
+++ Vis1.2.R
@@ -0,0 +1,51 @@
+library("MVA")
+## Diagrama de dispersión
+mlab <- "Empresas manufactureras con 20 o más trabajadores"
+plab <- "Tamaño de la población (censo de 1970) en miles"
+plot(popul ~ manu, data = USairpollution, xlab = mlab, ylab = plab)
+
+## Intro a Diagrama de caja bivariado
+plot(popul ~ manu, data = USairpollution, xlab = mlab, ylab = plab)
+rug(USairpollution$manu, side = 1)
+rug(USairpollution$popul, side = 2)
+
+# dispersión y densidades marginales
+layout(matrix(c(2, 0 , 1 , 3), nrow = 2, byrow = TRUE), widths = c(2, 1), heights = c(1, 2), respect = TRUE)
+#xlim <- with(USairpollution, range(manu)) * 1.1
+xlim <- range(USairpollution$manu) * 1.1
+plot(popul ~ manu, data = USairpollution, cex.lab = 0.9, xlab = mlab, ylab = plab, type="n", xlim=xlim)
+text(USairpollution$manu, USairpollution$popul, cex = 0.6, labels = abbreviate(row.names(USairpollution)))
+#with(USairpollution, text(manu, popul, cex = 0.6, labels = abbreviate(row.names(USairpollution))))
+hist(USairpollution$manu, main = "", xlim = xlim)
+#with(USairpollution, hist(manu, main = "", xlim = xlim))
+boxplot(USairpollution$popul)
+#with(USairpollution, boxplot(popul))
+
+## Diagrama de caja bivariado
+lab <- c("Chicago", "Detroit", "Cleveland", "Philadelphia", "Houston")
+outcity <- match(lab, rownames(USairpollution))
+x <- USairpollution[,c("manu", "popul")]
+bvbox(x, mtitle = "", xlab = mlab, ylab = plab)
+text(x$manu[outcity], x$popul[outcity], labels = lab, cex = 0.7, pos = c(2, 2, 4, 2, 2))
+
+## correlación
+with(USairpollution, cor(manu, popul))
+outcity1 <- match(c("Chicago", "Detroit", "Cleveland", "Philadelphia"), rownames(USairpollution)) 
+with(USairpollution, cor(manu[-outcity1],popul[-outcity1]))
+hull <- with(USairpollution, chull(manu, popul))
+with(USairpollution, plot(manu, popul, pch = 1, xlab = mlab, ylab = plab))
+with(USairpollution, polygon(manu[hull], popul[hull], density = 15, angle = 30))
+#cálculo de la correlación después de eliminar los puntos que definen el casco convexo
+with(USairpollution, cor(manu[-hull], popul[-hull]))
+
+### Matriz de gráficas de dispersión
+pairs(USairpollution, pch = ".", cex = 1.5)
+pairs(USairpollution, panel = function (x, y, ...) {
+	points (x, y, ...)
+	abline(lm(y ~ x), col = "grey")
+}, pch = ".", cex = 1.5)
+
+## matriz de correlación
+round(cor(USairpollution), 4)
+
+#Centrándonos en las correlaciones entre SO2 y las otras seis variables, vemos que la correlación entre SO2 y precip es muy pequeña y entre SO2 y predías es moderada. Pero los paneles relevantes en el diagrama de dispersión indican que el coeficiente de correlación que evalúa solo la relación lineal entre dos variables puede no ser adecuado aquí y que en un modelo de regresión lineal múltiple para los datos podrían considerarse efectos cuadráticos de predías y precip.
